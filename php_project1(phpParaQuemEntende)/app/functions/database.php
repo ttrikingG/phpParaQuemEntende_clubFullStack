264c21228b48c2d39b0e55c06acce113e676995c
Hailtom Garcia Dias <122056292+ttrikingG@users.noreply.github.com>
2023-12-29 22:15:33 -0300

    Update database.php

--- php_project1(phpParaQuemEntende)/app/functions/database.php
+++ php_project1(phpParaQuemEntende)/app/functions/database.php
@@ -4,8 +4,8 @@ function connect(){
     $pdo = new \PDO("mysql:host=localhost;
                     dbname=db_project1;
                     charset=utf8",
-                    'tomtom',
-                    '7244');
+                    '',
+                    '');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
